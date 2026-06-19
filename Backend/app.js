f3b3f4f9148d214972546568ac311528a4c9164d
Bhush_Ahire <159047235+Bhush8766@users.noreply.github.com>
2026-06-19 12:56:15 +0530

    Fix CORS

--- Backend/app.js
+++ Backend/app.js
@@ -1,24 +1,35 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 const connectToDb = require('./db/db');
 const userRoute = require('./routes/user.routes');
 const carRoute = require('./routes/car.routes');
-
 const cookieParser = require('cookie-parser');
 
 connectToDb();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://cars-selling-app-1.onrender.com"
+    ],
+    credentials: true
+  })
+);
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/',(req, res) => {
-    res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
+
 app.use('/users', userRoute);
 app.use('/car', carRoute);
 
