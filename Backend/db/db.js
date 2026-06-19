5bbcc80524e2f859b99e14100352c44e83a53d29
Bhush_Ahire <159047235+Bhush8766@users.noreply.github.com>
2026-06-19 13:51:21 +0530

    MongoDB Atlas setup

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const connectToDb = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB Connection Error:");
+    console.error(error.message);
   }
 };
 
