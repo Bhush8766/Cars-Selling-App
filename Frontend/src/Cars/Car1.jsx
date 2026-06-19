b91387dd7b8abf40d0559b26bd3bee70deb2b844
Bhush_Ahire <159047235+Bhush8766@users.noreply.github.com>
2026-06-19 14:01:21 +0530

    upload

--- Frontend/src/Cars/Car1.jsx
+++ Frontend/src/Cars/Car1.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavbarAfter from '../Components/NavbarAfter';
-import car1 from "../img/car1.jpg"; 
+import car1 from '../img/car1.jpg';
 
 const Car1 = () => {
   const navigate = useNavigate();
 
-  const handlePurchase = (event) => {
-    event.preventDefault();  // Prevent default anchor behavior
-    navigate('/purchase');   // Correct way to navigate programmatically
+  const handlePurchase = () => {
+    navigate('/purchase');
   };
 
   return (
     <>
       <NavbarAfter />
-      <section className="carssection container" id="carssection">
+
+      <section className="carssection container">
         <div className="carssection-img">
-          {/* <img src="./src/img/car1.jpg" alt="Car" /> */}
           <img src={car1} alt="Car" />
         </div>
+
         <div className="carssection-text">
           <div className="box">
-            <h2>Porche Car</h2>
-            <span>Rs. 1.99 Crore </span>
+            <h2>Porsche Car</h2>
+            <span>Rs. 1.99 Crore</span>
+
             <i className="bx bxs-star">(6 Reviews)</i>
+
             <br />
-            <a href="#" className="details">View Details</a>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod fugit nemo quae distinctio, mollitia aliquam!</p>
-            <button onClick={handlePurchase} className="btn">Buy Now</button>
+
+            <a href="#" className="details">
+              View Details
+            </a>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+
+            <button onClick={handlePurchase} className="btn">
+              Buy Now
+            </button>
           </div>
         </div>
       </section>
